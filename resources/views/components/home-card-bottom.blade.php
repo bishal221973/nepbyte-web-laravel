3c06ac9eba4cf056a15981ee34aee9b30c0d2e86
bishal221973 <bishalcodeslaravel@gmail.com>
2026-03-10 02:02:14 +0545

    Wrp

--- resources/views/components/home-card-bottom.blade.php
+++ resources/views/components/home-card-bottom.blade.php
@@ -1,20 +1,20 @@
 <div class="border-bottom-menu animate__animated animate__zoomIn">
 
 </div>
-<div class="d-flex align-items-center gap-2 justify-content-center text-white bottom-list mt-2">
-    <small>Strategy</small>
-    <i class="fa fa-arrow-right"></i>
-    <small>Branding</small>
-    <i class="fa fa-arrow-right"></i>
-    <small>Printing</small>
-    <i class="fa fa-arrow-right"></i>
-    <small>Content</small>
-    <i class="fa fa-arrow-right"></i>
-    <small>Marketing</small>
-    <i class="fa fa-arrow-right"></i>
-    <small>Development</small>
-    <i class="fa fa-arrow-right"></i>
-    <small>AI</small>
-    <i class="fa fa-arrow-right"></i>
-    <small>Scaling</small>
+<div class="d-flex  align-items-center gap-2 justify-content-center text-white bottom-list mt-2">
+    <small class="animate__animated animate__zoomInLeft">Strategy</small>
+    <i class="fa fa-arrow-right animate__animated animate__zoomInLeft"></i>
+    <small class="animate__animated animate__zoomInLeft">Branding</small>
+    <i class="fa fa-arrow-right animate__animated animate__zoomInLeft"></i>
+    <small class="animate__animated animate__zoomInLeft">Printing</small>
+    <i class="fa fa-arrow-right animate__animated animate__zoomInLeft"></i>
+    <small class="animate__animated animate__zoomInLeft">Content</small>
+    <i class="fa fa-arrow-right animate__animated animate__zoomInLeft"></i>
+    <small class="animate__animated animate__zoomInLeft">Marketing</small>
+    <i class="fa fa-arrow-right animate__animated animate__zoomInLeft"></i>
+    <small class="animate__animated animate__zoomInLeft">Development</small>
+    <i class="fa fa-arrow-right animate__animated animate__zoomInLeft"></i>
+    <small class="animate__animated animate__zoomInLeft">AI</small>
+    <i class="fa fa-arrow-right animate__animated animate__zoomInLeft"></i>
+    <small class="animate__animated animate__zoomInLeft">Scaling</small>
   </div>
