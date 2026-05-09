d72dee4b85c6926d15b10b19dc6ca7ff6f812d4e
bishal221973 <bishalcodeslaravel@gmail.com>
2026-05-09 12:55:57 +0545

    Sitemap

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ use App\Http\Controllers\SeoController;
 use App\Http\Controllers\TeamController;
 use App\Models\Brands;
 use Illuminate\Support\Facades\Route;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -109,4 +112,60 @@ Route::middleware(['auth'])->group(function () {
 });
 
 
-__DIR__.'/sitemap.php';
+
+Route::get('generate-sitemap',function(){
+
+Sitemap::create()
+    ->add(
+        Url::create('https://surkhetsoft.com/')
+            ->setLastModificationDate(Carbon::yesterday())
+            ->setPriority(1.0)
+    )
+    ->add(
+        Url::create('https://surkhetsoft.com/collaboration')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(1.0)
+    )
+     ->add(
+        Url::create('https://https://surkhetsoft.com/our-team')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(0.8)
+    )
+     ->add(
+        Url::create('https://surkhetsoft.com/strategy')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(0.8)
+    )
+     ->add(
+        Url::create('https://surkhetsoft.com/brands-development')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(0.8)
+    )
+     ->add(
+        Url::create('https://surkhetsoft.com/print-design')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(0.8)
+    )
+     ->add(
+        Url::create('https://surkhetsoft.com/content-production')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(0.8)
+    )
+     ->add(
+        Url::create('https://surkhetsoft.com/marketing')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(0.8)
+    )
+     ->add(
+        Url::create('https://surkhetsoft.com/development')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(0.8)
+    )
+     ->add(
+        Url::create('https://surkhetsoft.com/ai-automation')
+            ->setLastModificationDate(Carbon::now())
+            ->setPriority(0.8)
+    )
+    ->writeToFile(public_path('sitemap.xml'));
+});
+// __DIR__.'/sitemap.php';
